65120a55ed9713e048d573794ff0afb08873c349
Albert Schröder <sca21002@googlemail.com>
2016-04-06 10:19:02 +0200

    Open next deeper admin unitby click on a point
    Clicking on a point on the map get down to the next deeper admin unit
    or directly opens the image in case of a place with only one view.

--- root/app/scripts/controllers/views.js
+++ root/app/scripts/controllers/views.js
@@ -177,90 +177,105 @@ angular.module('vedutaApp')
 //      }
     });   
 
+
+    function isInSelection(unit, adminUnitSelected) {
+        if (!adminUnitSelected || !unit)  { return false; }
+        return unit.get(adminUnitSelected.admin) === adminUnitSelected.name;
+    }
+
+    function isOkTitles(titles, pids, years) {
+        return angular.isArray(titles) && 
+               angular.isArray(pids) && 
+               angular.isArray(years) &&
+               titles.length === pids.length && 
+               titles.length === years.length; 
+    }
+
+
+    function getViewfromFeature(feature) {
+        var adminUnit = $scope.admin;        
+        var view = {
+            title: adminUnitService.getFullName(
+                feature.get('name'), 
+                adminUnit, 
+                feature.get('adm')
+            ),
+            name: feature.get('name'), 
+            admin: adminUnit,
+            viewCount: getViewCount(feature), 
+            id: feature.get('id'), 
+        };
+        if (adminUnit === 'lkr' || adminUnit === 'gmd') {
+            view.regbez = feature.get('regbez'); 
+        }
+        if (adminUnit === 'gmd') {
+            view.lkr = feature.get('lkr');
+        }
+        return view;
+    }
+
+
     function updateList() {
 
         olData.getMap().then(function(map) {
             $scope.currentPage = 1;
             $scope.itemsPerPage = 3;	    
+            var adminUnit = $scope.admin;
+            var adminUnitSelected = $scope.adminUnitSelected;
 	        var view = map.getView();
             var extent = view.calculateExtent( map.getSize() );	    
-            var layers = map.getLayers();
-            layers.forEach(function(layer) {
-                if (layer.get('name') === 'views') {
-                    var source = layer.getSource();
-		            $scope.views = [];
-                    var rest = [];
-                    var features =  source.getFeaturesInExtent(extent);
-                    features.forEach(function(feature) {
-                        var pids;
-                        if ($scope.admin === 'place') {
-		                    var titles = angular.fromJson(feature.get('title'));
-			                pids   = angular.fromJson(feature.get('pid'));
-			                var years  = angular.fromJson(feature.get('year'));
-			                if (angular.isArray(titles) && angular.isArray(pids) && angular.isArray(years) &&
-                                titles.length === pids.length && titles.length === years.length) {
-			                    titles.forEach(function(title,i) {
-                                    if ($scope.adminUnitSelected && 
-                                        ($scope.admin === $scope.adminUnitSelected.admin && 
-                                         feature.get('name') === $scope.adminUnitSelected.name) ||     
-                                        ($scope.admin !== $scope.adminUnitSelected.admin && 
-                                         feature.get($scope.adminUnitSelected.admin) === $scope.adminUnitSelected.name)) {
-    		                            $scope.views.push({
-                                            title: title, 
-                                            icon: thumbnailURL(pids[i]), 
-                                            id: pids[i],
-                                            gmd: feature.get('gmd'),
-                                            lkr: feature.get('lkr'),
-                                            regbez: feature.get('regbez')
-                                        });			 
-                                        // console.log('Selected: ', title); 
-                                    }  else {
-    		                            rest.push({
-                                            title: title, 
-                                            icon: thumbnailURL(pids[i]), 
-                                            id: pids[i],
-                                            gmd: feature.get('gmd'),
-                                            lkr: feature.get('lkr'),
-                                            regbez: feature.get('regbez')
-                                        });			 
-                
-                                    }
-                                });    
-                            }				     
-                        } else {	
-                            pids   = angular.fromJson(feature.get('pid'));
-                            var index = getRandomPid(pids.length);
-                            var set = {
-                                title: adminUnitService.getFullName(
-                                    feature.get('name'), 
-                                    $scope.admin, 
-                                    feature.get('adm')
-                                ),
-                                name: feature.get('name'), 
-                                admin: $scope.admin,
-                                viewCount: getViewCount(feature), 
-                                id: feature.get('id'), 
-                                icon: thumbnailURL(pids[index]),
-                                pid: pids[index]
-                            };
-                            if ($scope.admin === 'lkr' || $scope.admin === 'gmd') {
-                                set.regbez = feature.get('regbez'); 
-                            }
-                            if ($scope.admin === 'gmd') {
-                                set.lkr = feature.get('lkr');
-                            }
-                            if ($scope.adminUnitSelected && feature.get($scope.adminUnitSelected.admin) === $scope.adminUnitSelected.name) {
-			                    $scope.views.push(set);
-                                // console.log('Selected: ', set.title); 
-                            } else {
-                                rest.push(set);
-                            }
-			            }     
-	                });
-                    $scope.views = $scope.views.concat(rest);
-                }		
+            getLayer('views').then(function(layer) {
+                if (!layer) {return;}
+                var source = layer.getSource();
+	            $scope.views = [];
+                var rest = [];
+                var features =  source.getFeaturesInExtent(extent);
+                features.forEach(function(feature) {
+                    var pids;
+                    if (adminUnit === 'place') {
+	                    var titles = angular.fromJson(feature.get('title'));
+		                pids   = angular.fromJson(feature.get('pid'));
+		                var years  = angular.fromJson(feature.get('year'));
+                        if (isOkTitles(titles, pids, years)) {
+		                    titles.forEach(function(title, i) {
+                                if (isInSelection(feature, adminUnitSelected)) {
+		                            $scope.views.push({
+                                        title: title, 
+                                        icon: thumbnailURL(pids[i]), 
+                                        id: pids[i],
+                                        gmd: feature.get('gmd'),
+                                        lkr: feature.get('lkr'),
+                                        regbez: feature.get('regbez')
+                                    });			 
+                                }  else {
+		                            rest.push({
+                                        title: title, 
+                                        icon: thumbnailURL(pids[i]), 
+                                        id: pids[i],
+                                        gmd: feature.get('gmd'),
+                                        lkr: feature.get('lkr'),
+                                        regbez: feature.get('regbez')
+                                    });			 
+            
+                                }
+                            });    
+                        }				     
+                    } else {	
+                        pids   = angular.fromJson(feature.get('pid'));
+                        var index = getRandomPid(pids.length);
+                        var view = getViewfromFeature(feature); 
+                        view.icon = thumbnailURL(pids[index]);
+                        view.pid = pids[index];
+                        if (isInSelection(feature, adminUnitSelected)) {
+		                    $scope.views.push(view);
+                        } else {
+                            rest.push(view);
+                        }
+		            }     
+                });
+                $scope.views = $scope.views.concat(rest);
+                $scope.totalViews = $scope.views.length;
 	        });	
-            $scope.totalViews = $scope.views.length;
 	    });        	
     }
 
@@ -375,35 +390,41 @@ angular.module('vedutaApp')
         selectedFeatures = [];
     }
 
+
+    function styleSelected(feature) {
+        return  new ol.style.Style({
+            text: new ol.style.Text({
+                text: feature.get('view_count').toString(),
+                fill: new ol.style.Fill({
+                    color: '#000'
+                }),
+            }),    
+            fill: new ol.style.Fill({
+                color: 'rgba(255, 100, 50, 0.3)'
+            }),
+            stroke: new ol.style.Stroke({
+                width: 2,
+                color: 'rgba(255, 100, 50, 0.8)'
+            }),
+            image: new ol.style.Circle({
+                fill: new ol.style.Fill({
+                    color: 'rgba(255,255,255,0.4)'
+                }),
+                stroke: new ol.style.Stroke({
+                    width: 5,
+                    color: '#fe3333'
+                }),
+                radius: 14
+            })
+        });
+    }
+    
+    
+    
     $scope.hover = function(view) {
         var view_id = view.id;
         getFeature($scope.admin, view_id).then(function(feature) {
-            var style = new ol.style.Style({
-                text: new ol.style.Text({
-                    text: feature.get('view_count').toString(),
-                    fill: new ol.style.Fill({
-                        color: '#000'
-                    }),
-                }),    
-                fill: new ol.style.Fill({
-                    color: 'rgba(255, 100, 50, 0.3)'
-                }),
-                stroke: new ol.style.Stroke({
-                    width: 2,
-                    color: 'rgba(255, 100, 50, 0.8)'
-                }),
-                image: new ol.style.Circle({
-                    fill: new ol.style.Fill({
-                        color: 'rgba(255,255,255,0.4)'
-                    }),
-                    stroke: new ol.style.Stroke({
-                        width: 5,
-                        color: '#fe3333'
-                    }),
-                    radius: 14
-                })
-            });
-            feature.setStyle(style);
+            feature.setStyle(styleSelected(feature));
             selectedFeatures.push(feature);
         });
     };
@@ -412,6 +433,26 @@ angular.module('vedutaApp')
     $scope.unhover = function() {
         unselectPreviousFeatures();
     };
+
+
+    olData.getMap().then(function(map) {
+        map.on("click", function(event) {
+            map.forEachFeatureAtPixel(event.pixel, function (feature) {
+                if ($scope.admin === 'place') {
+                    var pids =  angular.fromJson(feature.get('pid'));
+                    if (pids.length === 1) { 
+                        $window.open(digitoolService.getURL(pids[0]));
+                    }
+                } else {
+                    var view =  getViewfromFeature(feature);
+                    $scope.zoomIn(view);
+                }
+            }, null, function(layer) {
+                return layer.get('name') === 'views';
+            });
+        });
+    });
+
 
 //    function setCoordinateAndShow(coordinate, title, url, pid) {
 //        // Set position
@@ -425,16 +466,16 @@ angular.module('vedutaApp')
 //        $(overlay.getElement()).show(); 
 //    }
 
-//    olData.getMap().then(function(map) {           
-//        map.on('pointermove', function(event) {
-//            map.forEachFeatureAtPixel(event.pixel, function(feature) {
-//                var title = feature.get('title');
-//                var pid = feature.get('pid');
-//                var url = thumbnailURL(pid);
-//                var geometry = feature.getGeometry();
-//                var coordinates = geometry.getCoordinates();
-//                setCoordinateAndShow(coordinates, title, url, pid);
-//            });    
-//        });
-//    });
+    olData.getMap().then(function(map) {           
+        map.on('pointermove', function(event) {
+            var hit = map.forEachFeatureAtPixel(event.pixel, function(feature) {
+                feature.setStyle(styleSelected(feature));
+                selectedFeatures.push(feature);
+                return true;
+            }, null, function(layer) {
+                return layer.get('name') === 'views';
+            });
+            if (!hit) { unselectPreviousFeatures(); }
+        });  
+    });
 });
